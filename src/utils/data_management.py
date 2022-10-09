510b31ddd7752ab82d1cfe65090acd5d80df2cb2
Deepanshu Kaushik <deepanshukaushik1998@gmail.com>
2022-10-09 14:12:48 +0530

    csr matrix update in 2nd Stage

--- src/utils/data_management.py
+++ src/utils/data_management.py
@@ -35,7 +35,7 @@ def save_matrix(df, text_matrix, out_path):
     pid_matrix = sparse.csr_matrix (df.pid.astype(np.int64)).T
     label_matrix = sparse.csr_matrix (df.pid.astype(np.int64)).T
     
-    result = sparse.hstack([pid_matrix,label_matrix,text_matrix])
+    result = sparse.hstack([pid_matrix,label_matrix,text_matrix], format= 'csr')
     msg = f"The output matrix of shape {result.shape} is saved at {out_path}"
     logging.info(msg)
     joblib.dump(result,out_path)
