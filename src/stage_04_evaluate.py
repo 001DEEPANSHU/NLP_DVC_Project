760bb4b40c64a91c7f00e9e083573f72f8948589
Deepanshu Kaushik <deepanshukaushik1998@gmail.com>
2022-10-14 16:28:04 +0530

    evaluate stage created

---
+++ src/stage_04_evaluate.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories, save_json
+import sklearn.metrics as metrics 
+import math
+import numpy as np
+import joblib
+
+
+STAGE = "Evaluate" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def main(config_path, params_path):
+    ## read config files
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    
+    artifacts = config["artifacts"]
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    featurized_test_data_path = os.path.join(featurized_data_dir_path, artifacts["FEATURIZED_DATA_TEST"])
+
+    model_dir = artifacts["MODEL_DIR"]
+    model_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], model_dir)
+    model_name = artifacts["MODEL_NAME"]
+    model_path = os.path.join(model_dir_path, model_name)
+
+    model = joblib.load(model_path)
+    matrix = joblib.load(featurized_test_data_path)
+    labels = np.squeeze(matrix[:,1].toarray())
+    X = matrix[:,2:]
+
+    predict_by_class = model.predict_proba(X)
+    print(predict_by_class)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
